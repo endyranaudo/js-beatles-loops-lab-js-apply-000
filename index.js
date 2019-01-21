5f5fa971d75561390c49a06ffbd2e1ef97688a79
Endy Ranaudo <endyranaudo@gmail.com>
2019-01-21 18:00:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // add solution here
 function theBeatlesPlay (musician, instrument){
   var arr = [];
-  for (var i = 0; i < theBeatlesPlay.lenght)
+  for (var i = 0; i < musician.lenght; i++){
+    
+  }
 }

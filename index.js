244257d2637415b4ef8c770d81318cdbd87ed841
Endy Ranaudo <endyranaudo@gmail.com>
2019-01-21 17:56:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // add solution here
 function beatles (musician, instrument){
-  
+  var musician = ["John Lennon", "Ringo Star", "Paul McCartney", "George Harrison"];
+  var instrument = ["piano", "drums", "bass", "guitar"];
 }
